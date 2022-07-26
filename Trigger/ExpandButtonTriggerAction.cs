2fb6e0e81edd36f042cb39b5a94d25fdb84848f0
alexd <alexd.18dios@gmail.com>
2022-07-26 14:07:28 -0600

    Agregar archivos de proyecto.

---
+++ Trigger/ExpandButtonTriggerAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Trigger
+{
+    using Xamarin.Forms;
+    public class ExpandButtonTriggerAction : TriggerAction<Button>  
+    {
+        protected override async void Invoke(Button sender)
+        {
+            await sender.ScaleTo(0.95, 50, Easing.CubicOut);
+            await sender.ScaleTo(1, 50, Easing.CubicIn);
+        }
+    }
+}
